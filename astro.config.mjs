754b40af080824a30c68a225df7f440aded42d50
1182NPC <1182NPC@users.noreply.github.com>
2026-02-01 19:22:09 +0100

    Making References Work

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,22 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import remarkGfm from "remark-gfm";
 
 export default defineConfig({
   site: "https://astro-nano-demo.vercel.app",
-  integrations: [mdx(), sitemap(), tailwind()],
+  
+  // 1. Apply to standard .md files (THIS IS WHAT WAS MISSING)
+  markdown: {
+    remarkPlugins: [remarkGfm],
+  },
+
+  integrations: [
+    // 2. Apply to .mdx files
+    mdx({
+      remarkPlugins: [remarkGfm],
+    }),
+    sitemap(),
+    tailwind(),
+  ],
 });
