c3dc018b47b300e4ae9fa944517f1504b7529690
1182NPC <1182NPC@users.noreply.github.com>
2026-02-01 15:10:20 +0100

    Fixed Errors Photography Page

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,4 +10,16 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+// --- ADD THIS SECTION ---
+const photography = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    date: z.coerce.date(),
+    cover: z.string(),
+  }),
+});
+
+// --- CHANGE THIS LINE ---
+export const collections = { blog, photography };
